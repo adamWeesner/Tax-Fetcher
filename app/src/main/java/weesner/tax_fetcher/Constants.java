29f16aa0c24adf6cfc2998b7a3fa3e31f018ec43
Adam <alweesner@gmail.com>
2017-07-31 22:00:34 -0500

    type: fix, update
    removed stringToDouble function as it was not necessary
    updated assets json files for 2017
    updated javaDocs
    added constants.java
    updated strings.xml
    removed all hardcoded strings in TaxFetcher.java

---
+++ app/src/main/java/weesner/tax_fetcher/Constants.java
@@ -0,0 +1,31 @@
+package weesner.tax_fetcher;
+
+/**
+ * Created by Adam Weesner on 7/31/2017.
+ */
+
+public class Constants {
+    // the file names in assets folder to be used in retrieving values
+    public static final String MEDICARE = "medicare";
+    public static final String SOCIAL_SECURITY = "socialSecurity";
+    public static final String ALLOWANCES = "allowances";
+    public static final String FEDERAL_INCOME_TAX = "federalIncomeTax";
+    // English constants for retrieving allowances
+    public static final String PERIOD_TYPE_WEEKLY = "Weekly";
+    public static final String PERIOD_TYPE_BIWEEKLY = "Biweekly";
+    public static final String PERIOD_TYPE_SEMIMONTHLY = "Semimonthly";
+    public static final String PERIOD_TYPE_MONTHLY = "Monthly";
+    public static final String PERIOD_TYPE_QUARTERLY = "Quarterly";
+    public static final String PERIOD_TYPE_SEMIANNUAL = "Semiannual";
+    public static final String PERIOD_TYPE_ANNUAL = "Annual";
+    public static final String PERIOD_TYPE_DAILY = "Daily";
+    // English constants for married and single
+    public static final String MARITAL_STATUS_SINGLE = "Single";
+    public static final String MARITAL_STATUS_MARRIED = "Married";
+
+    public static final String JSON = ".json";
+    public static final String NO_MORE_THAN = "noMoreThan";
+    public static final String PLUS = "plus";
+    public static final String PERCENT = "percent";
+    public static final String WITHHELD = "withheld";
+}
--- app/src/main/java/weesner/tax_fetcher/TaxFetcher.java
+++ app/src/main/java/weesner/tax_fetcher/TaxFetcher.java
@@ -1,7 +1,6 @@
 package weesner.tax_fetcher;
 
 import android.content.Context;
-import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -11,29 +10,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import static weesner.tax_fetcher.Constants.ALLOWANCES;
+import static weesner.tax_fetcher.Constants.FEDERAL_INCOME_TAX;
+import static weesner.tax_fetcher.Constants.JSON;
+import static weesner.tax_fetcher.Constants.MEDICARE;
+import static weesner.tax_fetcher.Constants.NO_MORE_THAN;
+import static weesner.tax_fetcher.Constants.PERCENT;
+import static weesner.tax_fetcher.Constants.PERIOD_TYPE_ANNUAL;
+import static weesner.tax_fetcher.Constants.PERIOD_TYPE_BIWEEKLY;
+import static weesner.tax_fetcher.Constants.PERIOD_TYPE_DAILY;
+import static weesner.tax_fetcher.Constants.PERIOD_TYPE_MONTHLY;
+import static weesner.tax_fetcher.Constants.PERIOD_TYPE_QUARTERLY;
+import static weesner.tax_fetcher.Constants.PERIOD_TYPE_SEMIANNUAL;
+import static weesner.tax_fetcher.Constants.PERIOD_TYPE_SEMIMONTHLY;
+import static weesner.tax_fetcher.Constants.PERIOD_TYPE_WEEKLY;
+import static weesner.tax_fetcher.Constants.PLUS;
+import static weesner.tax_fetcher.Constants.SOCIAL_SECURITY;
+import static weesner.tax_fetcher.Constants.WITHHELD;
+
 /**
  * Created by alwee on 10/3/2016.
  */
 
 public class TaxFetcher {
-    // the file names in assets folder to be used in retrieving values
-    public static final String MEDICARE = "medicare";
-    public static final String SOCIAL_SECURITY = "socialSecurity";
-    public static final String ALLOWANCES = "allowances";
-    public static final String FEDERAL_INCOME_TAX = "federalIncomeTax";
-    // English constants for retrieving allowances
-    public static final String PERIOD_TYPE_WEEKLY = "Weekly";
-    public static final String PERIOD_TYPE_BIWEEKLY = "Biweekly";
-    public static final String PERIOD_TYPE_SEMIMONTHLY = "Semimonthly";
-    public static final String PERIOD_TYPE_MONTHLY = "Monthly";
-    public static final String PERIOD_TYPE_QUARTERLY = "Quarterly";
-    public static final String PERIOD_TYPE_SEMIANNUAL = "Semiannual";
-    public static final String PERIOD_TYPE_ANNUAL = "Annual";
-    public static final String PERIOD_TYPE_DAILY = "Daily";
-    // English constants for married and single
-    public static final String MARITAL_STATUS_SINGLE = "Single";
-    public static final String MARITAL_STATUS_MARRIED = "Married";
-
     /**
      * A helper function to retrieve the JSON file from the assets folder
      *
@@ -46,9 +45,7 @@ public class TaxFetcher {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(context.getResources().getAssets().open(fileToLoad)));
             String temp;
-            while ((temp = br.readLine()) != null) {
-                sb.append(temp);
-            }
+            while ((temp = br.readLine()) != null) sb.append(temp);
 
             br.close();
         } catch (IOException e) {
@@ -70,14 +67,14 @@ public class TaxFetcher {
         double ficaValue = 0;
         if (type.equals(MEDICARE) || type.equals(SOCIAL_SECURITY)) {
             try {
-                JSONObject ficaObject = loadJSONFromAsset(context, type + ".json");
+                JSONObject ficaObject = loadJSONFromAsset(context, type + JSON);
                 JSONObject ficaItems = ficaObject.getJSONObject(type);
                 ficaValue = ficaItems.getDouble(year);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         } else {
-            throw new IllegalArgumentException("Invalid type. Type can only be: " + MEDICARE + " or " + SOCIAL_SECURITY);
+            throw new IllegalArgumentException(String.format(context.getString(R.string.invalid_fica_type_error), MEDICARE, SOCIAL_SECURITY));
         }
         return ficaValue;
     }
@@ -107,24 +104,24 @@ public class TaxFetcher {
     /**
      * helper function to get the total amount that will be taken out of ones check for Medicare
      *
-     * @param context used to retrieve resources from the assets folder
-     * @param checkAmount  the gross amount of ones check needed to figure out how much gets taxed
-     * @param year    has to be 4 digit format eg: 2016
+     * @param context     used to retrieve resources from the assets folder
+     * @param checkAmount the gross amount of ones check needed to figure out how much gets taxed
+     * @param year        has to be 4 digit format eg: 2016
      * @return the percentage of ones check that is taken out by social security
      */
-    public static double getMedicareTax(Context context, double checkAmount, String year){
-        return checkAmount * (getMedicare(context, year)/100);
+    public static double getMedicareTax(Context context, double checkAmount, String year) {
+        return checkAmount * doubleToPercentage(getMedicare(context, year));
     }
 
     /**
      * helper function to get the total amount that will be taken out of ones check for Medicare
      *
-     * @param context used to retrieve resources from the assets folder
-     * @param checkAmount  the gross amount of ones check needed to figure out how much gets taxed
-     * @param year    has to be 4 digit format eg: 2016
+     * @param context     used to retrieve resources from the assets folder
+     * @param checkAmount the gross amount of ones check needed to figure out how much gets taxed
+     * @param year        has to be 4 digit format eg: 2016
      * @return the percentage of ones check that is taken out by social security
      */
-    public static double getMedicareTax(Context context, double checkAmount, int year){
+    public static double getMedicareTax(Context context, double checkAmount, int year) {
         return getMedicareTax(context, checkAmount, String.valueOf(year));
     }
 
@@ -153,24 +150,24 @@ public class TaxFetcher {
     /**
      * helper function to get the total amount that will be taken out of ones check for Social Security
      *
-     * @param context used to retrieve resources from the assets folder
-     * @param checkAmount  the gross amount of ones check needed to figure out how much gets taxed
-     * @param year    has to be 4 digit format eg: 2016
+     * @param context     used to retrieve resources from the assets folder
+     * @param checkAmount the gross amount of ones check needed to figure out how much gets taxed
+     * @param year        has to be 4 digit format eg: 2016
      * @return the percentage of ones check that is taken out by social security
      */
-    public static double getSocialSecurityTax(Context context, double checkAmount, String year){
-        return checkAmount * (getSocialSecurity(context, year)/100);
+    public static double getSocialSecurityTax(Context context, double checkAmount, String year) {
+        return checkAmount * doubleToPercentage(getSocialSecurity(context, year));
     }
 
     /**
      * helper function to get the total amount that will be taken out of ones check for Social Security
      *
-     * @param context used to retrieve resources from the assets folder
-     * @param checkAmount  the gross amount of ones check needed to figure out how much gets taxed
-     * @param year    has to be 4 digit format eg: 2016
+     * @param context     used to retrieve resources from the assets folder
+     * @param checkAmount the gross amount of ones check needed to figure out how much gets taxed
+     * @param year        has to be 4 digit format eg: 2016
      * @return the percentage of ones check that is taken out by social security
      */
-    public static double getSocialSecurityTax(Context context, double checkAmount, int year){
+    public static double getSocialSecurityTax(Context context, double checkAmount, int year) {
         return getSocialSecurityTax(context, checkAmount, String.valueOf(year));
     }
 
@@ -188,7 +185,7 @@ public class TaxFetcher {
                 periodType.equals(PERIOD_TYPE_MONTHLY) || periodType.equals(PERIOD_TYPE_QUARTERLY) || periodType.equals(PERIOD_TYPE_SEMIANNUAL) ||
                 periodType.equals(PERIOD_TYPE_ANNUAL) || periodType.equals(PERIOD_TYPE_DAILY)) {
             try {
-                JSONObject allowancesObject = loadJSONFromAsset(context, ALLOWANCES + ".json");
+                JSONObject allowancesObject = loadJSONFromAsset(context, ALLOWANCES + JSON);
                 JSONObject allowanceObject = allowancesObject.getJSONObject(ALLOWANCES);
                 JSONObject allowanceItems = allowanceObject.getJSONObject(year);
                 allowanceCost = allowanceItems.getDouble(periodType);
@@ -196,7 +193,7 @@ public class TaxFetcher {
                 e.printStackTrace();
             }
         } else {
-            throw new IllegalArgumentException("Period Type must be equal to one of the English constants provided starting with PERIOD_TYPE_ eg: PERIOD_TYPE_WEEKLY");
+            throw new IllegalArgumentException(context.getString(R.string.invalid_allowance_type_error));
         }
 
         return allowanceCost;
@@ -227,35 +224,35 @@ public class TaxFetcher {
         if (allowances <= 10) {
             return getAllowanceCost(context, periodType, year) * allowances;
         } else {
-            throw new IllegalArgumentException("The amount of allowances cannot exceed 10");
+            throw new IllegalArgumentException(context.getString(R.string.invalid_allowance_quantity_error));
         }
     }
 
     /**
      * function that gets the total amount of ones check that can be taxed
      *
-     * @param context    used to retrieve resources from the assets folder
-     * @param periodType needs to be an English constant provided starting with PERIOD_TYPE_
-     * @param checkAmount  the gross amount of ones check needed to figure out how much of it can be taxed
-     * @param allowances amount of allowances one has entered, can be 0 - 10 any other number will throw error
-     * @param year       has to be 4 digit format eg: 2016
+     * @param context     used to retrieve resources from the assets folder
+     * @param periodType  needs to be an English constant provided starting with PERIOD_TYPE_
+     * @param checkAmount the gross amount of ones check needed to figure out how much of it can be taxed
+     * @param allowances  amount of allowances one has entered, can be 0 - 10 any other number will throw error
+     * @param year        has to be 4 digit format eg: 2016
      * @return total cost of all allowances
      */
-    public static double getCanBeTaxedAmount(Context context, String periodType, double checkAmount, int allowances, String year){
+    public static double getCanBeTaxedAmount(Context context, String periodType, double checkAmount, int allowances, String year) {
         return checkAmount - getTotalAllowancesCost(context, periodType, allowances, year);
     }
 
     /**
      * function that gets the total amount of ones check that can be taxed
      *
-     * @param context    used to retrieve resources from the assets folder
-     * @param periodType needs to be an English constant provided starting with PERIOD_TYPE_
-     * @param checkAmount  the gross amount of ones check needed to figure out how much of it can be taxed
-     * @param allowances amount of allowances one has entered, can be 0 - 10 any other number will throw error
-     * @param year       has to be 4 digit format eg: 2016
+     * @param context     used to retrieve resources from the assets folder
+     * @param periodType  needs to be an English constant provided starting with PERIOD_TYPE_
+     * @param checkAmount the gross amount of ones check needed to figure out how much of it can be taxed
+     * @param allowances  amount of allowances one has entered, can be 0 - 10 any other number will throw error
+     * @param year        has to be 4 digit format eg: 2016
      * @return total cost of all allowances
      */
-    public static double getCanBeTaxedAmount(Context context, String periodType, double checkAmount, int allowances, int year){
+    public static double getCanBeTaxedAmount(Context context, String periodType, double checkAmount, int allowances, int year) {
         return getCanBeTaxedAmount(context, periodType, checkAmount, allowances, String.valueOf(year));
     }
 
@@ -274,32 +271,26 @@ public class TaxFetcher {
         double fitCost = 0;
         double canBeTaxed = checkAmount - getTotalAllowancesCost(context, periodType, allowances, year);
         try {
-            JSONObject federalIncomeTaxObject = loadJSONFromAsset(context, FEDERAL_INCOME_TAX + ".json");
+            JSONObject federalIncomeTaxObject = loadJSONFromAsset(context, FEDERAL_INCOME_TAX + JSON);
             JSONObject fitItems = federalIncomeTaxObject.getJSONObject(FEDERAL_INCOME_TAX);
             JSONObject fitItemsForYear = fitItems.getJSONObject(year);
             JSONArray fitItemForTypeAndStatus = fitItemsForYear.getJSONArray(periodType + "_" + maritalStatus);
             boolean needValue = true;
-            for (int k = 0; k < fitItemForTypeAndStatus.length(); k++) {
+            for (int i = 0; i < fitItemForTypeAndStatus.length(); i++) {
                 try {
-                    JSONObject fitQualifiers = fitItemForTypeAndStatus.getJSONObject(k);
-                    if (canBeTaxed <= stringToDouble(fitQualifiers.getString("noMoreThan"))) {
+                    JSONObject fitQualifiers = fitItemForTypeAndStatus.getJSONObject(i);
+                    if (canBeTaxed <= fitQualifiers.getDouble(NO_MORE_THAN)) {
                         if (needValue) {
                             needValue = false;
-                            double plus = fitQualifiers.getDouble("plus");
-                            double percent = fitQualifiers.getDouble("percent") / 100;
-                            double withheld = fitQualifiers.getDouble("withheld");
-                            fitCost = (canBeTaxed - withheld) * percent + plus;
+                            fitCost = federalIncomeTaxCost(canBeTaxed, fitQualifiers);
                         }
                     }
                 } catch (NumberFormatException nfe) {
                     if (needValue) {
                         needValue = false;
-                        if (nfe.getMessage().equals("Invalid double: \"max\"")) {
-                            JSONObject fitQualifiers = fitItemForTypeAndStatus.getJSONObject(k);
-                            double plus = fitQualifiers.getDouble("plus");
-                            double percent = fitQualifiers.getDouble("percent") / 100;
-                            double withheld = fitQualifiers.getDouble("withheld");
-                            fitCost = (canBeTaxed - withheld) * percent + plus;
+                        if (nfe.getMessage().equals(context.getString(R.string.invalid_double_error))) {
+                            JSONObject fitQualifiers = fitItemForTypeAndStatus.getJSONObject(i);
+                            fitCost = federalIncomeTaxCost(canBeTaxed, fitQualifiers);
                         }
                     }
                 }
@@ -325,8 +316,27 @@ public class TaxFetcher {
         return getFederalIncomeTax(context, checkAmount, maritalStatus, periodType, allowances, String.valueOf(year));
     }
 
-    private static double stringToDouble(String value) {
-        return Double.parseDouble(value);
+    /**
+     * helper method to get the amount of Federal Income Tax to be taken out of ones check
+     * @param canBeTaxed the amount of ones check that can be taxed
+     * @param qualifiers the JSONObject that contains the qualifiers for Federal Income Tax
+     * @return the amount of Federal Income Tax to be taken out
+     * @throws JSONException thrown if JSON information cannot be found
+     */
+    private static double federalIncomeTaxCost(double canBeTaxed, JSONObject qualifiers) throws JSONException {
+        double plus = qualifiers.getDouble(PLUS);
+        double percent = doubleToPercentage(qualifiers.getDouble(PERCENT));
+        double withheld = qualifiers.getDouble(WITHHELD);
+        return (canBeTaxed - withheld) * percent + plus;
+    }
+
+    /**
+     * helper function to get the percentage of based on the double value given from the JSON file
+     * @param amount amount to convert to percentage
+     * @return the percentage based on the double given
+     */
+    public static double doubleToPercentage(double amount){
+        return amount / 100;
     }
 }
 
